797cce9678d76fffb748d56510317ded7ec1e0bc
Leul tesfu <leultesfu24@gmail.com>
2025-07-04 13:42:43 +0300

    after merge bug fix

--- server.js
+++ server.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-dotenv.config({ path: './config.env' });
-const db = process.env.DATABASE_LOCAL;
-// const db = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
-
-// Allow requests from your frontend domain
-app.use(
-  cors({
-    origin: '*', // <-- change to your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
-
-mongoose
-  .connect(db)
-  .then(() => {
-    console.log('✅Database Connected successfully...');
-  })
-  .catch((err) => {
-    console.log('❌Connection Failed to the database');
-  });
-app.listen(port, () => {
-  console.log(`server listening in port ${port}`);
-});
-=======
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
@@ -47,6 +10,12 @@ const db = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  })
+);
 
 mongoose
   .connect(db)
@@ -59,4 +28,3 @@ mongoose
 app.listen(port, () => {
   console.log(`server listening in port ${port}`);
 });
->>>>>>> ef97297fe3fdd093badf1b1a8be97af4aed8b725
