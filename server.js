c9506b0790f0fc3613b90c0caec00cf26e645d6f
Leul tesfu <leultesfu24@gmail.com>
2025-07-07 21:55:09 +0300

    reversing back to remote database

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = require('./app');
 const port = process.env.PORT || 5000;
 
 dotenv.config({ path: './config.env' });
-const db = process.env.DATABASE_LOCAL;
-// const db = process.env.DATABASE.replace(
-// '<PASSWORD>',
-// process.env.DATABASE_PASSWORD
-// );
+// const db = process.env.DATABASE_LOCAL;
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(db)
